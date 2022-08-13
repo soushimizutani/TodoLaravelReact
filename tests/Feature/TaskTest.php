2b3a0a6a13ab0b08380a0126feed5c0bb3aad47a
soushimizutani <>
2022-08-13 23:29:02 +0900

    テストを追加

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -8,16 +8,18 @@ use App\Models\Task;
 
 class TaskTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * @test
      */
     public function 一覧を取得()
     {
         $tasks = Task::factory()->count(10)->create();
-        dd($tasks);
-        //$response = $this->getJson('api/tasks');
-        //$response = $this->get('/');
-        $response->assertOK();
+        $response = $this->getJson('api/tasks');
+        $response
+            ->assertOk()
+            ->assertJsonCount($tasks->count());
     }
 }
-//./vendor/bin/phpunit tests/Feature/TaskTest.php
+
+// ./vendor/bin/phpunit tests/Feature/TaskTest.php
