1bf0b21902168d1463693cd0e03603735105f96d
soushimizutani <>
2022-08-15 23:14:31 +0900

    バグ修正

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -94,4 +94,13 @@ class TaskController extends Controller
             ? response()->json($task)
             : response()->json([], 500);
     }
+
+    public function updateDone(Task $task, \Request $request)
+    {
+        $task->is_done = $request->is_done;
+
+        return $task->update()
+            ? response()->json($task)
+            : response()->json([], 500);
+    }
 }
